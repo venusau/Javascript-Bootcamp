59ec533200c32fd815f2640c9915be0b82505e29
Vicky Bhattacharya <vickybhattacharya19@gmail.com>
2024-03-08 11:34:28 +0530

    first class commit

--- LiveClass/1.BasicsOfJavascript/script.js
+++ LiveClass/1.BasicsOfJavascript/script.js
@@ -1,37 +1,37 @@
-console.log("hey there");
-var maths=100;
-var physics=60;
-var sum=maths+physics;
-console.log(sum);
+// console.log("hey there");
+// var maths=100;
+// var physics=60;
+// var sum=maths+physics;
+// console.log(sum);
 
-//variable ------------------------------------- naming 
-// In modern js we use let and const 
-// do not use var 
-// camel case , snake caseing and pascal casing 
-// in old js -> before es6 -> ecma script version 6 
-//modern js -> es6 and after 
-//in modern js let and const are introduced . before that we only had var 
-const pi= 3.14
-// i cannot change the constant variable 
-//canst abd; const variable must be initialised as well in the same line of declaration 
-let math=28 ;
-math =82 ;
-console.log("maths marks :", math);
-//so basically with let  ->  we can update the variable but with const  ->   we cannot 
+// //variable ------------------------------------- naming 
+// // In modern js we use let and const 
+// // do not use var 
+// // camel case , snake caseing and pascal casing 
+// // in old js -> before es6 -> ecma script version 6 
+// //modern js -> es6 and after 
+// //in modern js let and const are introduced . before that we only had var 
+// const pi= 3.14
+// // i cannot change the constant variable 
+// //canst abd; const variable must be initialised as well in the same line of declaration 
+// let math=28 ;
+// math =82 ;
+// console.log("maths marks :", math);
+// //so basically with let  ->  we can update the variable but with const  ->   we cannot 
 
-//var -> we can also update like let 
-let score=310;
+// //var -> we can also update like let 
+// let score=310;
 
-let str1="The score is 290";
-var str2="the score is 290 ";
-const str3=`the score is ${score} `;
-//i'll not be writting the score everytime , so for that i'll be usind ${} and the string should be enclosed 
-// withing -> `-----` -> backticks 
-//{} inside we can keep anything javascript 
+// let str1="The score is 290";
+// var str2="the score is 290 ";
+// const str3=`the score is ${score} `;
+// //i'll not be writting the score everytime , so for that i'll be usind ${} and the string should be enclosed 
+// // withing -> `-----` -> backticks 
+// //{} inside we can keep anything javascript 
 
 
-console.log(str1)
-console.log(str2)
-console.log(str3)
+// console.log(str1)
+// console.log(str2)
+// console.log(str3)
 
 
